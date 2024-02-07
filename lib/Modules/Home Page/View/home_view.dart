2a161a0eac036cdcd6de261e5fc92d66cb48d30f
dilosyja <wandilo123@gmail.com>
2024-02-07 13:21:43 +0700

    UI Landing and Login Page

--- lib/Modules/Home Page/View/home_view.dart
+++ lib/Modules/Home Page/View/home_view.dart
@@ -10,57 +10,50 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Presensi'),
-        centerTitle: true,
-        backgroundColor: Colors.blue[700],
-        foregroundColor: Colors.white,
-        automaticallyImplyLeading: false,
-      ),
       body: Obx(() => _controller.isLoading
           ? Center(child: CircularProgressIndicator())
           : SingleChildScrollView(
               child: Padding(
-                padding: const EdgeInsets.all(8.0),
+                padding: const EdgeInsets.only(top: 45.0),
                 child: Column(
                   children: [
                     if (_controller.currentPosition != null)
-                      Center(
-                        child: Column(
-                          children: [
-                            Text(
-                              'Latitude: ${_controller.currentPosition!.latitude}\nLongitude: ${_controller.currentPosition!.longitude}',
-                              style: TextStyle(fontSize: 18),
-                            ),
-                            SizedBox(height: 20),
-                            Text(
-                              'Distance to Office: ${_controller.calculateDistance(_controller.currentPosition, CompanyData.office)} meters',
-                              style: TextStyle(fontSize: 18),
-                            ),
-                            ElevatedButton(
-                              onPressed: _controller.handlePresensi,
-                              child: Text('Presensi'),
-                            ),
-                            SizedBox(height: 20),
-                            ElevatedButton(
-                              onPressed: _controller.launchOfficeOnMap,
-                              child: Text('Launch Office on Map'),
-                            ),
-                            SizedBox(height: 20),
-                            ElevatedButton(
-                              onPressed: () {
-                                // Navigate to the Presence History page
-                                Get.to(() => PresencePage());
-                              },
-                              child: Text('Presence History'),
-                            ),
-                            SizedBox(height: 20),
-                            ElevatedButton(
-                              onPressed: _controller.logout,
-                              child: Text('Log Out'),
-                            ),
-                          ],
-                        ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          CircleAvatar(radius: 50),
+                          Text(
+                            'Latitude: ${_controller.currentPosition!.latitude}\nLongitude: ${_controller.currentPosition!.longitude}',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                          SizedBox(height: 20),
+                          Text(
+                            'Distance to Office: ${_controller.calculateDistance(_controller.currentPosition, CompanyData.office)} meters',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                          ElevatedButton(
+                            onPressed: _controller.handlePresensi,
+                            child: Text('Presensi'),
+                          ),
+                          SizedBox(height: 20),
+                          ElevatedButton(
+                            onPressed: _controller.launchOfficeOnMap,
+                            child: Text('Launch Office on Map'),
+                          ),
+                          SizedBox(height: 20),
+                          ElevatedButton(
+                            onPressed: () {
+                              // Navigate to the Presence History page
+                              Get.to(() => PresencePage());
+                            },
+                            child: Text('Presence History'),
+                          ),
+                          SizedBox(height: 20),
+                          ElevatedButton(
+                            onPressed: _controller.logout,
+                            child: Text('Log Out'),
+                          ),
+                        ],
                       ),
                   ],
                 ),
