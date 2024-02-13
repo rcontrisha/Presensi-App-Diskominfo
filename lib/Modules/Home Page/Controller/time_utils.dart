40733fc65e658ee41e28208af35c9030cf47f7c1
dilosyja <wandilo123@gmail.com>
2024-02-13 14:19:50 +0700

    Update

---
+++ lib/Modules/Home Page/Controller/time_utils.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+import 'package:intl/intl.dart';
+import 'package:timezone/timezone.dart' as tzf;
+import 'package:flutter/animation.dart';
+
+class TimeUtils {
+  static Stream<String> getWIBTimeStream() async* {
+    while (true) {
+      final wibTimeZone = tzf.getLocation('Asia/Jakarta');
+      final wibDateTime = tzf.TZDateTime.now(wibTimeZone);
+      yield DateFormat('HH:mm').format(wibDateTime);
+      await Future.delayed(Duration(seconds: 1));
+    }
+  }
+
+  static Animation<double> getOpacityAnimation(AnimationController controller) {
+    return Tween<double>(
+      begin: 1.0,
+      end: 0.0,
+    ).animate(controller);
+  }
+}
